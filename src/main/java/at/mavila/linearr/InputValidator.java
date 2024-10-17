3ca2a1f3834f1c40ba76fc7be6c433a00988abe9
Marco Tulio Ávila Cerón <marcotavilac@pm.me>
2024-10-17 10:32:30 +0200

    none `Refactor InputValidator to improve readability and maintainability.`

--- src/main/java/at/mavila/linearr/InputValidator.java
+++ src/main/java/at/mavila/linearr/InputValidator.java
@@ -21,21 +21,43 @@ public final class InputValidator {
    */
   public static Result wrapParameters(List<BigDecimal> x, List<BigDecimal> y, BigDecimal w, BigDecimal b) {
     //Validate each parameter for non-null and non-empty, if not throw an IllegalArgumentException
-    if (CollectionUtils.isEmpty(x)) {
-      throw new IllegalArgumentException("x is empty");
-    }
-    if (CollectionUtils.isEmpty(y)) {
-      throw new IllegalArgumentException("y is empty");
-    }
+    validateArrayX(x);
+    validateArrayY(y);
     //Validate that x and y have the same size
-    if (x.size() != y.size()) {
-      throw new IllegalArgumentException("x and y have different sizes");
-    }
+    checkArraySizes(x, y);
 
-    //Validate w and b for non-null and return the valid values
+    return createResult(w, b);
+  }
+
+  /**
+   * Validate w and b for non-null and return the valid values.
+   *
+   * @param w weight.
+   * @param b bias.
+   * @return the valid values.
+   */
+  private static Result createResult(BigDecimal w, BigDecimal b) {
     return Result.builder()
         .wValid(Objects.requireNonNull(w, "w is null"))
         .bValid(Objects.requireNonNull(b, "b is null"))
         .build();
+  }
+
+  private static void checkArraySizes(List<BigDecimal> x, List<BigDecimal> y) {
+    if (x.size() != y.size()) {
+      throw new IllegalArgumentException("x and y have different sizes");
+    }
+  }
+
+  private static void validateArrayY(List<BigDecimal> y) {
+    if (CollectionUtils.isEmpty(y)) {
+      throw new IllegalArgumentException("y is empty");
+    }
+  }
+
+  private static void validateArrayX(List<BigDecimal> x) {
+    if (CollectionUtils.isEmpty(x)) {
+      throw new IllegalArgumentException("x is empty");
+    }
   }
 }
